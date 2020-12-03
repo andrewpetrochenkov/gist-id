f73a494c876d7ff1cea6ede06b925f13ce6c8936
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 23:54:51 +0300

    2020:12:03 23:54

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gist-id',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/gist-id']
